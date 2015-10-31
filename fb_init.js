ead9c5117bf3394bf676884fb2798acc11f083c9
Daniel Colceag <daniel.colceag@gmail.com>
2015-10-31 23:38:00 +0200

    [Emotionify-16] Facebook API :: Configure Facebook Initialization with the Facebook App Configuration. #16
    - Added cookie usage config to Facebook configuration
    - Added function for JSON file read with callback
    - Refactored Facebook initialization into one function that receives the JSON configuration file as a parameter
    - Refactored whole JavaScript file

--- fb_init.js
+++ fb_init.js
@@ -1,32 +1,52 @@
 window.fbAsyncInit = function() {
-    FB.init({
-      appId      : 'xxxxxxxxx', // App ID
-      status     : true, // check login status
-      cookie     : true, // enable cookies to allow the server to access the session
-      xfbml      : true  // parse XFBML
+    // Read configuration file then populate the FB init script with the read configuration.
+    var url = "app.json",
+        getJSONConfig = function (path, callback) {
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function () {
+                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                    var data = JSON.parse(xmlhttp.responseText);
+                    if (callback) callback(data);
+                }
+            };
+            xmlhttp.open("GET", path, true);
+            xmlhttp.send();
+        },
+        init = function (config) {
+            FB.init({
+                appId      : config.facebook.appId, // App ID
+                status     : config.facebook.status, // check login status
+                cookie     : config.facebook.cookie, // enable cookies to allow the server to access the session
+                xfbml      : config.facebook.xfbml  // parse XFBML
+            });
+
+
+            FB.Event.subscribe('auth.authResponseChange', function(response) {
+                console.log("Received response from Facebook Server.");
+                // SUCCESS
+                if (response.status === 'connected') {
+                    console.log("Response was successful.")
+                    document.getElementById("message").innerHTML +=  "<br>Connected to Facebook";
+                } else
+                // FAILURE
+                if (response.status === 'not_authorized') {
+                    console.log("Failure authorizing! Check configuration file and try again!");
+                    document.getElementById("message").innerHTML +=  "<br>Failed to Connect";
+                } else
+                // UNKNOWN ERROR
+                {
+                    console.log("Unknown error occurred! Check configuration file and try again!");
+                    document.getElementById("message").innerHTML +=  "<br>Logged Out";
+                }
+            });
+        };
+
+    // Get JSON Config File
+    getJSONConfig(url, function(config) {
+        // Config file was loaded
+        console.log("Application configuration file loaded. Preparing initialization...");
+        // Initialize Application
+        init(config);
     });
-    
-    
-	FB.Event.subscribe('auth.authResponseChange', function(response) 
-	{
- 	 if (response.status === 'connected') 
-  	{
-  		document.getElementById("message").innerHTML +=  "<br>Connected to Facebook";
-  		//SUCCESS
-  		
-  	}	 
-	else if (response.status === 'not_authorized') 
-    {
-    	document.getElementById("message").innerHTML +=  "<br>Failed to Connect";
-
-		//FAILED
-    } else 
-    {
-    	document.getElementById("message").innerHTML +=  "<br>Logged Out";
-
-    	//UNKNOWN ERROR
-    }
-	});	
-	
-    };
+};
 
