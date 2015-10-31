f5070a983565f475d1b2a22849c097caa98243aa
Codescu Radu <codra27@gmail.com>
2015-10-31 21:07:52 +0200

    Function for facebook logout

---
+++ fb_logout.js
@@ -0,0 +1,13 @@
+function Logout()
+	{
+		FB.logout(function(){document.location.reload();});
+	}
+
+
+(function(d){
+     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement('script'); js.id = id; js.async = true;
+     js.src = "//connect.facebook.net/en_US/all.js";
+     ref.parentNode.insertBefore(js, ref);
+   }(document));
