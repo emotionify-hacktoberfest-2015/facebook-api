82879c3a8251679a2791a1d555d2ba890e59009f
Margretor <mihalache.octavian@gmail.com>
2015-10-31 18:39:33 +0200

    Facebook initialization script

---
+++ fb_init.js
@@ -0,0 +1,32 @@
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : 'xxxxxxxxx', // App ID
+      status     : true, // check login status
+      cookie     : true, // enable cookies to allow the server to access the session
+      xfbml      : true  // parse XFBML
+    });
+    
+    
+	FB.Event.subscribe('auth.authResponseChange', function(response) 
+	{
+ 	 if (response.status === 'connected') 
+  	{
+  		document.getElementById("message").innerHTML +=  "<br>Connected to Facebook";
+  		//SUCCESS
+  		
+  	}	 
+	else if (response.status === 'not_authorized') 
+    {
+    	document.getElementById("message").innerHTML +=  "<br>Failed to Connect";
+
+		//FAILED
+    } else 
+    {
+    	document.getElementById("message").innerHTML +=  "<br>Logged Out";
+
+    	//UNKNOWN ERROR
+    }
+	});	
+	
+    };
+
