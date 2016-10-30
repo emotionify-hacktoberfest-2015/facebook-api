f69cf7c7f3ce6aba8cca94f9d83ca681f0844577
danielmihai <daniel.colceag@gmail.com>
2016-10-30 20:30:17 +0200

    Modified logout and getUserInfo scripts

--- fb_logout.js
+++ fb_logout.js
@@ -1,13 +1,11 @@
-function Logout()
-	{
-		FB.logout(function(){document.location.reload();});
-	}
-
+function logout() {
+  FB.logout(function(){document.location.reload();});
+}
 
 (function(d){
-     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement('script'); js.id = id; js.async = true;
-     js.src = "//connect.facebook.net/en_US/all.js";
-     ref.parentNode.insertBefore(js, ref);
-   }(document));
+  var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+  if (d.getElementById(id)) {return;}
+  js = d.createElement('script'); js.id = id; js.async = true;
+  js.src = "//connect.facebook.net/en_US/all.js";
+  ref.parentNode.insertBefore(js, ref);
+}(document));
