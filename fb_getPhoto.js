c3b1fd5b155ecdc86e2edf111bf375468b39eb28
danielmihai <daniel.colceag@gmail.com>
2016-10-30 20:12:12 +0200

    Cleaned up a bit

--- fb_getPhoto.js
+++ fb_getPhoto.js
@@ -1,8 +1,6 @@
-function getPhoto()
-	{
-	  FB.api('/me/picture?type=normal', function(response) {
-
-		  var str="<br/><b>Pic</b> : <img src='"+response.data.url+"'/>";
-	  	  document.getElementById("status").innerHTML+=str;
-	  	  	    
-    });
+function getPhoto() {
+  FB.api('/me/picture?type=normal', function(response) {
+    var str="<br/><b>Pic</b> : <img src='"+response.data.url+"'/>";
+      document.getElementById("status").innerHTML+=str;
+  });
+};
