c14e1d4783af26bc1e36ab05a5747e276e5eb0a3
danielmihai <daniel.colceag@gmail.com>
2016-10-30 20:19:16 +0200

    Cleaned up a bit

--- fb_init.js
+++ fb_init.js
@@ -1,52 +1,55 @@
 window.fbAsyncInit = function() {
     // Read configuration file then populate the FB init script with the read configuration.
-    var url = "app.json",
-        getJSONConfig = function (path, callback) {
-            var xmlhttp = new XMLHttpRequest();
-            xmlhttp.onreadystatechange = function () {
-                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                    var data = JSON.parse(xmlhttp.responseText);
-                    if (callback) callback(data);
-                }
-            };
-            xmlhttp.open("GET", path, true);
-            xmlhttp.send();
-        },
-        init = function (config) {
-            FB.init({
-                appId      : config.facebook.appId, // App ID
-                status     : config.facebook.status, // check login status
-                cookie     : config.facebook.cookie, // enable cookies to allow the server to access the session
-                xfbml      : config.facebook.xfbml  // parse XFBML
-            });
+    var url = "app.json";
+    var getJSONConfig = function (path, callback) {
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.onreadystatechange = function () {
+          if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+              var data = JSON.parse(xmlhttp.responseText);
+              if (callback) callback(data);
+          }
+      };
+      xmlhttp.open("GET", path, true);
+      xmlhttp.send();
+    };
 
+    // Initialize Application
+    var init = function (config) {
+      // Login Application to Facebook
+      FB.init({
+          appId      : config.facebook.appId, // App ID
+          status     : config.facebook.status, // check login status
+          cookie     : config.facebook.cookie, // enable cookies to allow the server to access the session
+          xfbml      : config.facebook.xfbml  // parse XFBML
+      });
 
-            FB.Event.subscribe('auth.authResponseChange', function(response) {
-                console.log("Received response from Facebook Server.");
-                // SUCCESS
-                if (response.status === 'connected') {
-                    console.log("Response was successful.")
-                    document.getElementById("message").innerHTML +=  "<br>Connected to Facebook";
-                } else
-                // FAILURE
-                if (response.status === 'not_authorized') {
-                    console.log("Failure authorizing! Check configuration file and try again!");
-                    document.getElementById("message").innerHTML +=  "<br>Failed to Connect";
-                } else
-                // UNKNOWN ERROR
-                {
-                    console.log("Unknown error occurred! Check configuration file and try again!");
-                    document.getElementById("message").innerHTML +=  "<br>Logged Out";
-                }
-            });
-        };
+      // Subscribe to Authentication Events
+      FB.Event.subscribe('auth.authResponseChange', function(response) {
+          console.log("Received response from Facebook Server.");
+          // SUCCESS
+          if (response.status === 'connected') {
+              console.log("Response was successful.")
+              document.getElementById("message").innerHTML +=  "<br>Connected to Facebook";
+          } else 
+          // FAILURE
+          if (response.status === 'not_authorized') {
+            console.log("Failure authorizing! Check configuration file and try again!");
+            document.getElementById("message").innerHTML +=  "<br>Failed to Connect";
+          } else { 
+          // UNKNOWN ERROR
+            console.log("Unknown error occurred! Check configuration file and try again!");
+            document.getElementById("message").innerHTML +=  "<br>Logged Out";
+          }
+      });
+    };
 
     // Get JSON Config File
-    getJSONConfig(url, function(config) {
-        // Config file was loaded
-        console.log("Application configuration file loaded. Preparing initialization...");
-        // Initialize Application
-        init(config);
-    });
+  var getJSONConfig(url, function(config) {
+    // Config file was loaded
+    console.log("Application configuration file loaded. Preparing initialization...");
+    
+    // Initialize Application
+    init(config);
+  });
 };
 
--- getGallery.js
+++ getGallery.js
@@ -1,17 +1,21 @@
-function getGallery(){
-
-            FB.api('/me/albums',  function(resp) {
-                //Log.info('Albums', resp);
-                var ul = document.getElementById('albums/photos');
-                for (var i=0, l=resp.data.length; i<l; i++){
-                    var
-                        album = resp.data[i],
-                        li = document.createElement('li'),
-                        a = document.createElement('a');
-                    a.innerHTML = album.name;
-                    a.href = album.link;
-                    li.appendChild(a);
-                    ul.appendChild(li);
-                }
-            });
-        }
+function getGallery() {
+  FB.api('/me/albums',  function(resp) {
+    //Log.info('Albums', resp);
+    var ul = document.getElementById('albums/photos');
+    var album = null;
+    var li;
+    var a;
+    for (var i=0, l=resp.data.length; i<l; i++){
+      album = resp.data[i];
+      // Create HTML Elements to be appended
+      li = document.createElement('li');
+      a = document.createElement('a');
+      // Populate with data
+      a.innerHTML = album.name;
+      a.href = album.link;
+      // Append Elements to List
+      li.appendChild(a);
+      ul.appendChild(li);
+    }
+  });
+}
